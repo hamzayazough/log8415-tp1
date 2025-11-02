197aa193767193566f74eaf33cc649cdab7c1c7d
Michael Le <orangerasengan25@gmail.com>
2025-11-02 18:45:39 -0500

    Added more instructions as to how the user can view the results of word count experiment

--- TP2/run_word_count_test.sh
+++ TP2/run_word_count_test.sh
@@ -17,5 +17,7 @@ fi
 echo "Deploying Word Count Test Infrastructure"
 python src/word_count/word_count_setup.py
 
-echo "Deployment complete! Now just wait for the tests to run."
+echo "Deployment complete!"
+echo "To check the progress of the tests, ssh into the instance and use the following command: cat ../../var/log/cloud-init-output.log"
+echo "To check the performance results, wait until tests are done and check for comparison_hadoop_linux.png, comparison_hadoop_spark.png and timings.csv in /home/ubuntu/results/"
 echo "Cleanup: python src/word_count/word_count_teardown.py"
